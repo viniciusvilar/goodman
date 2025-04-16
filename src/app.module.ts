62ab5e9d9ac2905192064b7dd11c2a3e703a0301
Vinícius Vilar <viniciusvillarma7x@gmail.com>
2025-04-16 17:07:43 -0300

    Configurando docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true
-  }), ProductsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_ROOT_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }), 
+    ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
